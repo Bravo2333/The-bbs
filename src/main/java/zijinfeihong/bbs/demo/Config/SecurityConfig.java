9883253c23106af395bd8e32fb5e08bb12d8a4e8
Bravo2333 <2087369435@qq.com>
2020-08-02 22:38:34 +0800

    the beginning!01

---
+++ src/main/java/zijinfeihong/bbs/demo/Config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package zijinfeihong.bbs.demo.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String[] NO_AUTH_LIST = {
+            "/register",
+            "/home"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().cors()
+                .and().formLogin().loginPage("/login").loginProcessingUrl("/login")
+            .and().logout().logoutUrl("/logout").permitAll()
+                .and().authorizeRequests().antMatchers(NO_AUTH_LIST).permitAll()
+                .and().authorizeRequests().anyRequest().authenticated();
+}}
