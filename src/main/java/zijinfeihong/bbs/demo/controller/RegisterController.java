47cc02ec3fa3d926e75100899fcc6c109562ae1a
Bravo2333 <2087369435@qq.com>
2020-08-03 21:54:42 +0800

    04

--- src/main/java/zijinfeihong/bbs/demo/controller/RegisterController.java
+++ src/main/java/zijinfeihong/bbs/demo/controller/RegisterController.java
@@ -1,4 +1,17 @@
 package zijinfeihong.bbs.demo.controller;
 
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class RegisterController {
+        @PostMapping("/register")
+        public String register(@Param("Username")String username,
+                               @Param("account")String account,
+                               @Param("password")String password,
+                               @Param("email")String email,
+                               @Param("identification")String identification){
+
+            return "注册成功";
+        }
+
 }
