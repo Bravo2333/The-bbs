5c9a5883d21c603b2407101edd9c9b5929bea77b
IGFeng <2218785142@qq.com>
2020-08-10 16:48:17 +0800

    搞了一下

--- src/main/java/zijinfeihong/bbs/demo/dao/UserDao.java
+++ src/main/java/zijinfeihong/bbs/demo/dao/UserDao.java
@@ -3,12 +3,18 @@ package zijinfeihong.bbs.demo.dao;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 import zijinfeihong.bbs.demo.entity.Users;
 
 @Mapper
+@Repository
 public interface UserDao {
     @Select("Select * from user where username=#{username}")
     Users userChecker(String username);
     @Insert("insert into user (email,username,password,headpath) values(#{email},#{username},#{password},#{headpath})")
     Boolean userRegister(Users user);
+
+    int updateHeadPath(Users user);
+
+
 }
